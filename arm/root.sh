d5076e0fb14bf855fbc03b711ad3a07aff34366f
Roger Ortiz <rogerortizleal@gmail.com>
2019-11-05 19:09:53 +0100

    Imporve the tool and update mtk-su binaries.

---
+++ arm/root.sh
@@ -0,0 +1,28 @@
+##########################################################################################
+# Root Script
+##########################################################################################
+
+# Mount system read-write.
+mount -o remount -rw /system
+
+# Backup stock app_process.
+cp /system/bin/app_process /system/bin/app_process_original
+cp /system/bin/app_process32 /system/bin/app_process_original32
+
+# Copy su files.
+cp /data/local/tmp/su /system/xbin/su
+mv /data/local/tmp/su /system/xbin/daemonsu
+cp /data/local/tmp/supolicy /system/xbin/
+cp /data/local/tmp/libsupol.so /system/lib/
+
+# Set correct permissions.
+chmod 0755 /system/xbin/su
+chcon u:object_r:system_file:s0 /system/xbin/su
+chmod 0755 /system/xbin/daemonsu
+chcon u:object_r:system_file:s0 /system/xbin/daemonsu
+
+# Start the daemon.
+daemonsu --auto-daemon
+
+# Start SuperSU app.
+am start -a android.intent.action.MAIN -n eu.chainfire.supersu/.MainActivity
