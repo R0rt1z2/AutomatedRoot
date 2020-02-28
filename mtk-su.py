83d6910d6177b906232ffb8dd53bad575977794a
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-29 00:23:55 +0100

    AutomatedRoot: Bump to 5.0!
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- mtk-su.py
+++ mtk-su.py
@@ -7,11 +7,14 @@ from sys import platform as _platform
 # By R0rt1z2
 # All the credits goes to diplomatic for create his excellent MTK-SU!
 
-# Define clean variable for various OS.
 if _platform == "linux":
     clean = "clear"
 elif _platform == "win64" or "win32":
     clean = "cls"
+
+def get_var_from_system(command):
+        var = Popen(f'{command}', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+        return var
 
 def push_file(file, target):
         os.system("adb push {} {}".format(file, target))
@@ -20,184 +23,167 @@ def get_prop(prop, name):
         name = Popen(f'adb shell getprop {prop}', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
         return name
 
-def menu():
+def check_devices():
+        devices = get_var_from_system("adb devices")
+        sys.stdout.write("\n[?] Waiting for the device...")
+        while True:
+              if len(devices) == 24:
+                 devices = get_var_from_system("adb devices")
+                 sys.stdout.write("."); sys.stdout.flush()
+                 time.sleep(1)
+              else:
+                 print("\n[+] Found device!\n")
+                 if "offline" in devices or "unauthorized" in devices:
+                     print("[!] ERROR: Device is unauthorized or offline!\n")
+                     sys.exit(1)
+                 break
+
+def make_line(title, subtitle, margin):
+         line_len = len("| {}: {}".format(title, subtitle))
+         to_add = margin - line_len - 1
+         h = ''.join(["   " + "| {}: {}".format(title, subtitle)] + [ ' ' *to_add ] + ['|'])
+         print(h)
+
+def print_device_info():
+         ''' This needs a cleaner way! '''
+         device = get_prop("ro.product.model", "model")
+         platform = get_prop("ro.hardware", "platform")
+         arch = get_prop("ro.product.cpu.abi", "arch")
+         android_ver = get_prop("ro.build.version.release", "android")
+         brand = get_prop("ro.product.manufacturer", "brand")
+         selinux_status = get_var_from_system("adb shell getenforce")
+         root_present = get_var_from_system("adb shell which su")
+         margin = ''.join(["   " + '-' *50])
+         print(margin)
+         make_line("DEVICE", device, 50)
+         make_line("BRAND", brand, 50)
+         make_line("ARCH", arch, 50)
+         make_line("PLATFORM", platform, 50)
+         make_line("ANDROID VERSION", android_ver, 50)
+         make_line("SELINUX STATUS", selinux_status, 50)
+         make_line("ROOT?", root_present, 50)
+         print(margin)
+
+def print_banner():
+         print("   __          _                        _           _   __    ___  ___  _____")
+         print("  /_ \   _   _| |_ ___  _ __ ___   __ _| |_ ___  __| | /__\  /___\/___\/__   \ ")
+         print(" //_\ \ | | | | __/ _ \| '_ ` _ \ / _` | __/ _ \/ _` |/ \// //  ///  //  / /\/")
+         print("/  _   \| |_| ||   (_) | | | | | | (_| | ||  __/ (_| / _  \/ \_// \_//  / /   ")
+         print("\_/ \_/ \__,_|\__ \___/|_| |_| |_|\__,_|\__\___|\__,_\/ \_/\___/\___/   \/ ")
+                                                                           
+
+def show_menu():
         os.system(clean)
-        print("\t Big thanks to diplomatic")
-        print("\t1 - Root the Device")
-        print("\t2 - Spawn Root Shell")
-        print("\t3 - Unroot the device")
-        print("\t4 - Exit")
+        print_banner()
+        print("\n1 - Root the Device")
+        print("\n2 - Spawn Root Shell")
+        print("\n3 - Unroot the device")
+        print("\n4 - Exit")
 
 while True:
-        #show menu
-        menu()
+        show_menu()
 
-        #user sets option
-        option = input("Select an option >> ")
+        option = input("\n[?] Select an option >> ")
 
-        if option=="1":
-             # Clean before continue
+        if option is "1":
              os.system(clean)
-             print("Detecting device information....")
-             print("--------------------------------------")
-             model = get_prop("ro.product.model", "model")
-             platform = get_prop("ro.hardware", "platform")
+             print_banner()
+             print("\n[?] Getting device information...")
+
+             print_device_info()
+
              arch = get_prop("ro.product.cpu.abi", "arch")
-             android = get_prop("ro.build.version.release", "android")
-             brand = get_prop("ro.product.manufacturer", "brand")
-             selinux = Popen('adb shell getenforce', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-             supersu = Popen('adb shell pm list packages', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-             root = Popen('adb shell which su', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-             if "su" in root:
-                   root == "yes"
-                   pass
-             else:
-                   root = "no"
-                   pass
-             print("Device: " + model)
-             print("Brand: " + brand)
-             print("ARCH: " + arch)
-             print("Platform: " + platform)
-             print("Android: " + android)
-             print("SELinux status: " + selinux)
-             print("Root: " + root)
-             print("--------------------------------------")
+             platform = get_prop("ro.hardware", "platform")
+
              if "mt81" or "mt67" or "mt6595" or "mt6580" in platform:
-               pass
+                pass
              else:
-               wait = input("Incompatible CPU! BYE!")
-               break
+                print("[!] Unsupported CPU! mtk-su is not compatible with {}!\n".format(platform))
+                break
+                sys.exit(1)
              if "arm64-v8a" in arch:
-                print("Detected arm64 arch.. Pushing arm64 mtk-su & files")
-                push_file("arm64/mtk-su", "/data/local/tmp")
-                push_file("arm64/root.sh", "/data/local/tmp")
-                push_file("arm64/su", "/data/local/tmp")
-                push_file("arm64/supolicy", "/data/local/tmp")
-                push_file("arm64/libsupol.so", "/data/local/tmp")
-                print("--------------------------------------")
-                print("Pushed files succsefully!")
-                print("--------------------------------------")
+                print("[?] Pushing files...\n")
+                Popen('adb shell mkdir /data/local/tmp/arm', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+                Popen('adb shell mkdir /data/local/tmp/arm64', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+                push_file("files/arm64/mtk-su", "/data/local/tmp/arm64")
+                push_file("files/common/root.sh", "/data/local/tmp")
+                push_file("files/arm64/su", "/data/local/tmp/arm64")
+                push_file("files/arm64/supolicy", "/data/local/tmp/arm64")
+                push_file("files/arm64/libsupol.so", "/data/local/tmp/arm64")
+                push_file("files/arm/mtk-su", "/data/local/tmp/arm")
+                push_file("files/arm/su", "/data/local/tmp/arm")
+                push_file("files/arm/supolicy", "/data/local/tmp/arm")
+                push_file("files/arm/libsupol.so", "/data/local/tmp/arm")
+
+                supersu = get_var_from_system("adb shell pm list packages")
+
                 if "supersu" in supersu:
-                   print("SuperSU already installed... Skip the install")
+                   print("\n[?] SuperSU it's already installed...\n")
                    pass
                 else:
-                   print("Installing SuperSU...")
-                   call("adb install files/SuperSU.apk",shell=True)
+                   print("[?] Installing SuperSU...\n")
+                   call("adb install files/common/SuperSU.apk",shell=True)
                    pass
-                print("--------------------------------------")
-                print("Starting Root Process...")
-                call("adb shell chmod 755 /data/local/tmp/mtk-su",shell=True)
+                print("[?] Starting Root Process...\n")
+                call("adb shell chmod 755 /data/local/tmp/arm/mtk-su",shell=True)
+                call("adb shell chmod 755 /data/local/tmp/arm64/mtk-su",shell=True)
                 call("adb shell chmod 755 /data/local/tmp/root.sh",shell=True)
-                call('adb shell /data/local/tmp/mtk-su -c "/data/local/tmp/root.sh"',shell=True)
-                print("--------------------------------------")
-                wait = input("PRESS ENTER TO CONTINUE.")
-                print("--------------------------------------")
-                os.system(clean)
-             elif "armeabi-v7a" in arch:
-                print("Detected armv7 arch.. Pushing armv7 mtk-su & files")
-                push_file("arm/mtk-su", "/data/local/tmp")
-                push_file("arm/root.sh", "/data/local/tmp")
-                push_file("arm/su", "/data/local/tmp")
-                push_file("arm/supolicy", "/data/local/tmp")
-                push_file("arm/libsupol.so", "/data/local/tmp")
-                print("--------------------------------------")
-                print("Pushed files succsefully!")
-                print("--------------------------------------")     
-                if "supersu" in supersu:
-                   print("SuperSU already installed... Skip the install")
-                   pass
-                else:
-                   print("Installing SuperSU...")
-                   call("adb install files/SuperSU.apk",shell=True)
-                   pass
-                print("--------------------------------------")
-                print("Starting Root Process...")
-                call("adb shell chmod 755 /data/local/tmp/mtk-su",shell=True)
-                call("adb shell chmod 755 /data/local/tmp/root.sh",shell=True)
-                call('adb shell /data/local/tmp/mtk-su -c "/data/local/tmp/root.sh"',shell=True)
-                print("--------------------------------------")
-                wait = input("PRESS ENTER TO CONTINUE.")
-                print("--------------------------------------")
 
+                if "arm64" in arch:
+                    call('adb shell /data/local/tmp/arm64/mtk-su -c "/data/local/tmp/root.sh"',shell=True)
+                elif "armeabi-v7a" in arch:
+                    call('adb shell /data/local/tmp/arm/mtk-su -c "/data/local/tmp/root.sh"',shell=True)
 
-        elif option=="2":
+                ("[?] Press enter to continue\n")
+
+        elif option is "2":
                os.system(clean)
-               print("ROOT SHELL SPAWNER")
-               arch = Popen('adb shell getprop ro.product.cpu.abi', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               print("Spawning Root Shell... \n For exit type 'exit' or Control+C for terminate the script")
+               print_banner()
+               Popen('adb shell mkdir /data/local/tmp/arm', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+               Popen('adb shell mkdir /data/local/tmp/arm64', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+               arch = get_prop("ro.product.cpu.abi", "arch")
+               print("\n[?] Spawning Root Shell...\n")
+               print("[?] For exit type 'exit' or press Control+C for terminate the script\n")
                if "arm64-v8a" in arch:
-                   call("adb push arm64/mtk-su /data/local/tmp",shell=True)
-                   call("adb shell chmod 755 /data/local/tmp/mtk-su",shell=True)
-                   call("adb shell /data/local/tmp/mtk-su",shell=True)
-                   wait = input("PRESS ENTER TO CONTINUE.")
+                   call("adb push files/arm64/mtk-su /data/local/tmp/arm64",shell=True)
+                   call("adb shell chmod 755 /data/local/tmp/arm64/mtk-su",shell=True)
+                   call("adb shell /data/local/tmp/mtk-su -c '/system/bin/sh'",shell=True)
                    os.system(clean)
                elif "armeabi-v7a" in arch:
-                   call("adb push arm/mtk-su /data/local/tmp",shell=True)
-                   call("adb shell chmod 755 /data/local/tmp/mtk-su",shell=True)
-                   call("adb shell /data/local/tmp/mtk-su",shell=True)
-                   wait = input("PRESS ENTER TO CONTINUE.")
+                   call("adb push files/arm/mtk-su /data/local/tmp/arm",shell=True)
+                   call("adb shell chmod 755 /data/local/tmp/arm/mtk-su",shell=True)
+                   call("adb shell /data/local/tmp/mtk-su -c '/system/bin/sh'",shell=True)
                    os.system(clean)
 
-        elif option=="3":
+        elif option is "3":
                os.system(clean)
-               print("Detecting device information....")
-               print("--------------------------------------")
-               verity = Popen('adb shell getprop ro.boot.veritymode', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               mtk = Popen('adb shell getprop debug.mtklog.netlog.Running', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               device = Popen('adb shell getprop ro.product.model', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               platform = Popen('adb shell getprop ro.hardware', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               platform2 = Popen('adb shell getprop ro.hardware', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               arch = Popen('adb shell getprop ro.product.cpu.abi', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               android = Popen('adb shell getprop ro.build.version.release', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               brand = Popen('adb shell getprop ro.product.manufacturer', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               selinux = Popen('adb shell getenforce', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               supersu = Popen('adb shell pm list packages | grep chainfire', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
-               root = Popen('adb shell which su', shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True).stdout.read().strip().decode('utf-8')
+               print("\n[?] Getting device information...")
+
+               print_device_info()
+               root = get_var_from_system("adb shell which su")
                if "su" in root:
                    root = "yes"
                    pass
                else:
                    root = "no"
                    pass
-               print("Device: " + device)
-               print("Brand: " + brand)
-               print("ARCH: " + arch)
-               print("Platform: " + platform2)
-               print("Android: " + android)
-               print("SELinux status: " + selinux)
-               print("Root Status: " + root)
-               print("--------------------------------------")
                if "no" in root:
-                   print("You don't have root installed. How come you want to unroot it?")
+                   print("[-] Your device doesn't seems to be rooted!\n")
                    break
                else:
-                   print("Pushing unroot script & mtk-su...")
-                   if "abi" in arch:
-                      arch = "arm"
-                      print ("Pushing files...")
-                      os.system("adb push arm/unroot.sh /data/local/tmp")
-                      print("Setting correct permissions...")
-                      os.system("adb shell chmod 755 /data/local/tmp/*")
-                      print("Starting the unroot process")
-                      os.system("adb shell su -c './data/local/tmp/unroot.sh'")
-                      os.system("sleep 5")
-                   elif "arm64" in arch:
-                      arch = "arm64"
-                      print ("Pushing files...")
-                      os.system("adb push arm64/unroot.sh /data/local/tmp")
-                      print("Setting correct permissions...")
-                      os.system("adb shell chmod 755 /data/local/tmp/*")
-                      print("Starting the unroot process")
-                      os.system("adb shell su -c './data/local/tmp/unroot.sh'")
+                   print("[?] Pushing unroot script...\n")
+                   os.system("adb push files/common/unroot.sh /data/local/tmp")
+                   print("[?] Setting correct permissions...\n")
+                   os.system("adb shell chmod 755 /data/local/tmp/unroot.sh")
+                   print("[?] Starting the unroot process...\n")
+                   os.system("adb shell su -c '/data/local/tmp/unroot.sh'")
+                   input("[?] Press enter to continue\n")
 
-
-        elif option=="4":
-              os.system(clean)
-              print("BYE BYE")
-              time.sleep(0.5)
+        elif option is "4":
               os.system(clean)
               break
+              exit(0)
 
         else:
-               print("")
-               input("Incorrect option...\nPress any key to continue..")
+               input("[!] {}: invalid option. Press any key to continue...\n".format(option))
+
