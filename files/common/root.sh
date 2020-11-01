0fc5081a50e62d7d7055e3c53cecb6d15864f0e1
R0rt1z2 <rogerortizleal@gmail.com>
2020-11-01 21:05:05 +0100

    Remove fireos check.
    * Useless.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- files/common/root.sh
+++ files/common/root.sh
@@ -6,37 +6,6 @@ DEVICE=$(getprop ro.product.device) # Product model (i.e: suez, douglas, etc...)
 HARDWARE=$(getprop ro.hardware) # CPU (i.e: mt8163, mt8173, mt6735, etc...)
 ARCH=$(getprop ro.product.cpu.abi) # ARCH (i.e: armeabi-v7a, arm64-v8a, mips, etc...)
 
-# If device is "amazon-branded" check the FireOS version
-if [ $BRAND == "Amazon" ]; then
-    echo "[?] This is an Amazon Device..."
-
-    echo "[?] Checking FireOS version..."
-
-    FOS_VER=$(getprop ro.build.version.name | awk -F '[()]' '{print $2}')
-    FOS_NUMBER=$(getprop ro.build.version.fireos)
-
-    echo "[?] Your device seems to be in FireOS $FOS_VER"
-    if [ $DEVICE == "suez" ] || [ $DEVICE == "douglas" ] && [ $FOS_VER -gt 636558520 ]; then
-	   echo "[-] FireOS 5.6.4.0 build 636558520 and up are not supported in Fire 7th gen devices!"
-	   exit 1
-    elif [ $DEVICE == "giza" ] && [ $FOS_VER -gt 626533320 ]; then
-	   echo "[-] FireOS 5.3.6.4 build 626533320 and up are not supported in Fire 6th gen devices!"
-	   exit 1
-    elif [ $DEVICE == "sloane" ]; then
-	   echo "[-] Fire TV 2 (2015) sloane is not supported!"
-	   exit 1
-    elif [ $DEVICE == "karnak" ] && [ $FOS_NUMBER -gt "6.3.0.1" ]; then
-	   echo "[-] FireOS 6.3.0.1 and up are not supported in Fire HD8 (2018)!"
-	   exit 1
-    elif [ $DEVICE == "maverick" ] && [ $FOS_NUMBER -gt "7.3.1.0" ]; then
-	   echo "[-] FireOS 7.3.1.0 and up are not supported in Fire HD10 (2019)!"
-	   exit 1
-    elif [ $DEVICE == "mustang" ] && [ $FOS_NUMBER -gt "6.3.1.2" ]; then
-	   echo "[-] FireOS 6.3.1.2 and up are not supported in Fire 7 (2019)!"
-	   exit 1
-    fi
-fi
-
 # Check if the script was launched from a root shell
 ID=$(id | grep uid=0\(root\))
 
