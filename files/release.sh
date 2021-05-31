e7ae087e608459a8d186df4309cf63e99fdb0562
R0rt1z2 <rogerortizleal@gmail.com>
2021-05-31 19:07:50 +0200

    AutomatedRoot: add script to create releases
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

---
+++ files/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../
+
+rm -rf release >/dev/null 2>&1
+
+if [ ! -f files/arm/mtk-su ] || [ ! -f files/arm64/mtk-su ] ; then
+	echo "Missing mtk-su binaries in files/"
+	exit 1
+fi
+
+mkdir release
+mkdir release/files
+
+cp files/{MTK-SU.bat,MTK-SU.sh} release/
+chmod a+x release/*.sh
+
+cp -r files/{arm,arm64,common} release/files/
+chmod a+x release/files/common/*.sh
+dos2unix release/files/common/*.sh
+
+cp {mtk-su.py,README.md,LICENSE} release/
